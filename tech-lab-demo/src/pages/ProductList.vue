e389b96d2f6bc3aa0783bde268cee01d65775ffc
gonglong <wushan_gong_2006@163.com>
2018-01-01 23:08:53 +0800

    技术调研: vue-virtual-scroll-list --- 无限循环 + 监听scroll事件 + 确认第一条数据。 ps: 该插件满足当前业务需求， thanks

--- tech-lab-demo/src/pages/ProductList.vue
+++ tech-lab-demo/src/pages/ProductList.vue
@@ -4,12 +4,19 @@
   <plugin-header></plugin-header>
   <plugin-nav></plugin-nav>
   <p>Hello, {{name}} This is Product Page!</p>
-  <div class="" v-if="data.length > 0">
-    <div class="" v-for="item in data">
+  <div class="">
+    <button type="button" name="button" v-on:click="getList">add Data</button>
+  </div>
+  <div class="comm-scroll-data" v-if="data.length > 0">
+    <virtual-list :size="50" :remain="6" :onscroll="scrollOnscroll" :tobottom="scrollTobottom" >
+    <div class="comm-list" v-for="(item, index) in data" :key="index">
       <div class="">
-        list {{item.name}}
+         <span>{{item.name}} -- </span>
+         <span>{{item.price}} --</span>
+         <span>{{index}}</span>
       </div>
     </div>
+  </virtual-list>
   </div>
 </div>
 </template>
@@ -18,24 +25,30 @@
 import PluginHeader from '../components/PluginHeader'
 import PluginNav from '../components/PluginNav'
 import axios from 'axios'
+import virtualList from 'vue-virtual-scroll-list'
 
 export default {
   data: function () {
     return {
       name: 'GongLong',
       data: [{
-      "name": "牛奶",
-      "price": "35",
-      "seriaNo": "s10001"
-    }]
+        name: '牛奶',
+        price: '35',
+        seriaNo: 's10001'
+      }]
     }
   },
   components: {
     PluginHeader,
-    PluginNav
+    PluginNav,
+    'virtual-list': virtualList
   },
   mounted: function () {
+    console.log('[life circle] --> mounted!')
     this.getList()
+  },
+  destroyed: function () {
+    console.log('[life circle] --> destroyed!')
   },
   methods: {
     getList: function () {
@@ -49,7 +62,6 @@ export default {
         responseType: 'json'
       })
       .then(function (response) {
-
         console.log('[getList]--then!')
         that.data = that.data.concat(response.data.list)
       })
@@ -63,10 +75,28 @@ export default {
         }
       })
       // axion -- end.
+    },
+
+    scrollTotop: function () {
+      console.log('scrollTotop')
+    },
+    scrollTobottom: function () {
+      console.log('scrollTobottom')
+    },
+    scrollOnscroll: function (event, data) {
+      // stringify
+      console.log('scrollOnscroll, data: ' + JSON.stringify(data))
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+.comm-list{
+  box-sizing: border-box;
+}
+.comm-scroll-data{
+  height: 300px;
+  box-sizing: border-box;
+}
 </style>
